e8584105b15465195854835651e5c6c4d5ea2984
Patrick Rooney <patrickrooney09@gmail.com>
2023-03-18 13:09:41 -0400

    finished exercises for chapter 2

--- chapter2ex/script.js
+++ chapter2ex/script.js
@@ -13,3 +13,37 @@ const triangle = (hash = "#", param) => {
   }
 };
 triangle("#", "#");
+
+const fizzBuzz = () => {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) exercise2.innerHTML += `fizzbuzz<br />`;
+    if (i % 3 === 0) exercise2.innerHTML += `fizz<br />`;
+    if (i % 5 === 0) {
+      exercise2.innerHTML += `buzz<br />`;
+    } else {
+      exercise2.innerHTML += `${i}<br />`;
+    }
+  }
+};
+fizzBuzz();
+
+const chessBoard = (num) => {
+  let board = [];
+  for (let i = 0; i < num; i++) {
+    board.push([]);
+  }
+  for (let i = 0; i < num; i++) {
+    let currentRow = board[i];
+    for (let j = 0; j < num; j++) {
+      if (j % 2 === 0 && i % 2 !== 0) currentRow.push(" ");
+      if (j % 2 === 0 && i % 2 === 0) currentRow.push("#");
+      if (j % 2 !== 0 && i % 2 !== 0) currentRow.push("#");
+      if (j % 2 !== 0 && i % 2 === 0) currentRow.push(" ");
+    }
+  }
+  board.map((currentRow) => {
+    console.log(...currentRow);
+    exercise3.innerHTML += `${currentRow.join("")}<br />`;
+  });
+};
+chessBoard(8);
